745bf8f4ba2ba0977324b8efc0554b4c672cb57a
Izzet Can Karakus <izzetcankarakus@gmail.com>
2020-04-24 23:51:34 +0300

    added some of the homework 2

---
+++ HW2/src/hw2/Q1.java
@@ -0,0 +1,17 @@
+package hw2;
+
+public class Q1 {
+	public static void main(String[] args) {
+		
+		// not a valid syntax for declaring more than one variable 
+		// on the same line. Java expects statement terminator character.
+		
+		double num1, int num2 = 0;
+		
+		int inum1, inum2;
+		int inum3, inum4 = 8;
+		int inum5 = 5, inum6 = 6;
+		
+	}
+
+}
