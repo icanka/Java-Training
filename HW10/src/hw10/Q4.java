a61f5691d3c3f737403e24d66060e9771cf06d54
izzetcan <izzetcankarakus@gmail.com>
2020-06-15 23:19:53 +0300

    Added first of the incremental update  HW10

---
+++ HW10/src/hw10/Q4.java
@@ -0,0 +1,25 @@
+package hw10;
+
+import java.util.ArrayList;
+
+public class Q4 {
+	
+
+	public static void main(String[] args) {
+		int[] crossword [] = new int[10][20];
+		
+		
+		// Correctly, and without trying to access out of bounds
+		// this loop iterates through the two-dimensional array.
+		// It only iterates through the half of the every array in the
+		// second dimension though.
+		for(int i=0; i<crossword.length; i++)
+			for(int j=0; j<crossword.length; j++)
+				crossword[i][j] = 'x';
+		// This line has to be changed. There is no size() method for arrays
+		// we need to use the length member variable instead.
+		System.out.println(crossword.size());
+		
+	}
+
+}
